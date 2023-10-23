2ef715a6571fe1070c9ca453793c9fe715d327e8
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2023-10-23 16:20:33 +0530

    Update function overloading.cpp

--- compile time polymorphism/function overloading.cpp
+++ compile time polymorphism/function overloading.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int volume(double r, int h){
+    return(3.14 * r *r *h);
+}
+int volume(int a){
+    return (a * a * a);
+}
+int volume (int l, int b, int h){
+    return (l*b*h);
+}
+
+int main() {
+    
+    cout<<"The volume of cuboid of 3, 7 and 6 is "<<volume(3, 7, 6)<<endl;
+    cout<<"The volume of cylinder of radius 3 and height 6 is "<<volume(3, 6)<<endl;
+    cout<<"The volume of cube of side 3 is "<<volume(3)<<endl;
+    return 0;
+}
