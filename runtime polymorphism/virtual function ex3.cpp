04881a5cbd8f2079f5ca98858a686954c5aabd2d
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2023-12-07 11:05:17 +0530

    Update virtual function ex3.cpp

--- runtime polymorphism/virtual function ex3.cpp
+++ runtime polymorphism/virtual function ex3.cpp
@@ -3,18 +3,13 @@ using namespace std;
 class base {
 public:
 	virtual void print() { cout << "print base class\n"; }
-
 	void show() { cout << "show base class\n"; }
 };
 
 class derived : public base {
 public:
-	void print(){
-	    cout << "print derived class\n"; 
-	}
-	void show(){
-	    cout << "show derived class\n"; 
-	}
+	void print(){ cout << "print derived class\n"; }
+	void show(){  cout << "show derived class\n"; }
 };
 int main(){
 	base* bpoint;
